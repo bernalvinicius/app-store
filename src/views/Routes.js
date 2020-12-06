04bf312d1393d8ebe983951bebb7236744a25195
bernalvinicius <vini_bernal@hotmail.com>
2020-12-06 20:49:23 +0000

    Setup responsive base

--- src/views/Routes.js
+++ src/views/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./Home";
+import Resp from "./Resp";
 
 const Routes = () => {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route path="/resp" component={Resp} />
     </Router>
   );
 };
