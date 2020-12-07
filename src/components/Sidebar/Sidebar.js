1e684ba3d3018c197ee60f67fc947261afe5f699
bernalvinicius <vini_bernal@hotmail.com>
2020-12-07 21:59:04 +0000

    Sidebar Component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <>
+      <h2>Categories</h2>
+
+      <ul className="nav-menu">
+        <li className="active">
+          <a href="/">Channels</a>
+        </li>
+        <li>
+          <a href="/">Dialer</a>
+        </li>
+        <li>
+          <a href="/">Optimization</a>
+        </li>
+        <li>
+          <a href="/">Reporting</a>
+        </li>
+        <li>
+          <a href="/">Voice Analytics</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Sidebar;
