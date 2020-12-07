8ddbdaf951be9487e77d83b36cb1943933b1f8d1
bernalvinicius <vini_bernal@hotmail.com>
2020-12-07 22:15:25 +0000

    Apps Component

--- src/components/Apps/Apps.js
+++ src/components/Apps/Apps.js
@@ -1,7 +1,107 @@
 import React from "react";
+import "./Apps.scss";
 
 const Apps = () => {
-  return <div>Apps</div>;
+  return (
+    <ul>
+      <li>
+        <div class="app-item">
+          <div class="box-info">
+            <div class="box-info--content">
+              <div class="description">
+                <h1>Voice Report</h1>
+                <p>Calls reporting and analytics of your calls.</p>
+              </div>
+              <div class="tags">
+                <span>Voice Analytics</span> / <span>Reporting</span> /{" "}
+                <span>Optimization</span>
+              </div>
+            </div>
+            <div class="box-info--footer">
+              <ul>
+                <li>
+                  <span>Trial</span>{" "}
+                  <h3>
+                    Free<sup></sup>
+                  </h3>
+                </li>
+                <li>
+                  <span>Professional</span>{" "}
+                  <h3>
+                    35.00<sup>€</sup>
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="app-item">
+          <div class="box-info">
+            <div class="box-info--content">
+              <div class="description">
+                <h1>Voice Report</h1>
+                <p>Calls reporting and analytics of your calls.</p>
+              </div>
+              <div class="tags">
+                <span>Voice Analytics</span> / <span>Reporting</span> /{" "}
+                <span>Optimization</span>
+              </div>
+            </div>
+            <div class="box-info--footer">
+              <ul>
+                <li>
+                  <span>Trial</span>{" "}
+                  <h3>
+                    Free<sup></sup>
+                  </h3>
+                </li>
+                <li>
+                  <span>Professional</span>{" "}
+                  <h3>
+                    35.00<sup>€</sup>
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="app-item">
+          <div class="box-info">
+            <div class="box-info--content">
+              <div class="description">
+                <h1>Voice Report</h1>
+                <p>Calls reporting and analytics of your calls.</p>
+              </div>
+              <div class="tags">
+                <span>Voice Analytics</span> / <span>Reporting</span> /{" "}
+                <span>Optimization</span>
+              </div>
+            </div>
+            <div class="box-info--footer">
+              <ul>
+                <li>
+                  <span>Trial</span>{" "}
+                  <h3>
+                    Free<sup></sup>
+                  </h3>
+                </li>
+                <li>
+                  <span>Professional</span>{" "}
+                  <h3>
+                    35.00<sup>€</sup>
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 export default Apps;
